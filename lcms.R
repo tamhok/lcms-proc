a3953dfccc49160f774d3bc204be3496e86c2f4a
Hok Hei Tam <tamhok@gmail.com>
2014-09-15 07:57:57 -0400

    Initial commit

--- lcms.R
+++ lcms.R
@@ -1,12 +1,38 @@
 library(xlsx)
+library(reshape)
 source("~/hht_dataproc/R/hht_dataproc.R")
 
 load.file  <- function(filename) {
-	read.csv(filename, header=TRUE) 
+	print(filename)
+	data = read.csv(filename, header = FALSE, stringsAsFactors = FALSE) 
+	headers = as.character(data[1,])
+	#Figure out which columns are what acc = grep("Acc", headers)
+	samples = grep("RZA|SLG", headers)
+	
+	pdata = data[-1, c(acc, samples)]
+	colnames(pdata) = data[1, c(acc, samples)]
+	print(colnames(pdata))
+	extras = data.frame(run = rep(basename(filename), nrow(pdata)))
+	if(length(strsplit(colnames(pdata)[2], " ")[[1]]) == 3) {
+		cnames = c("mat", "size",  "id")
+		extras = data.frame(ie = rep(NA, nrow(pdata)), run = extras$run)
+	} else {
+		cnames = c("mat", "size", "ie", "id")
+	}
+	pdata = melt(pdata, id = 1)
+	pdata = data.frame(value = pdata$value, acc = pdata[,1], 
+		colsplit(pdata$variable, split = " ", names = cnames))
+	return(cbind(pdata, extras))
 }
 
 load.files  <- function() {
-	files = list.files(
-		"~/Dropbox/MELTON CELL MANUSCRIPT DATA/FIles to plot for heat map/", 
-		   pattern = "csv")
+	
+	filebase = "~/Dropbox/MELTON CELL MANUSCRIPT DATA/FIles to plot for heat map/"
+	files = list.files(filebase, pattern = "csv", full.names = TRUE)
+	datas = lapply(files, load.file)  
+	return(Reduce(rbind, datas))
+}
+
+lcms.test  <-  function() {
+	filename = "~/Dropbox/MELTON CELL MANUSCRIPT DATA/FIles to plot for heat map/140903_58_OV_sample1_123.csv"
 }
